ea762f31cedf8abc1222336e4c9b0bf7513c1910
EugeniaS96 <156593480+EugeniaS96@users.noreply.github.com>
2025-05-04 14:24:56 +0300

    Update Stack.java

--- src/main/java/org/example/Stack.java
+++ src/main/java/org/example/Stack.java
@@ -1,10 +1,8 @@
 package org.example;
 
 public class Stack {
-    //у стека обычно фикс размер (5)
    static int max_size = 5;
    static int size = 0;
-   //для хранения эл стека исп массив, равный 5
    static int[] data = new int[max_size];
 
    public static void main(String[] args) {
@@ -19,43 +17,27 @@ public class Stack {
        System.out.println(pop());
        print();
     }
-//удаляем эл из стека и возаращаем его пользователю, что он мог увидеть этот эл
+
     static int pop() {
-       //проверяем, а если стек пуст? отображаем ошибку
         if(size == 0) {
             throw new RuntimeException("Stack is empty!");
         }
-        //берем верхний эл стека - эл с индексом size (самый посл эл)
-        // т к size увелич после того, как мы кладем значен эл (в методе push size++
-        //идет после data[size] = value), нам нужно обращаться size - 1
-        //иначе мы получим ячейку, кот еще не заполнена значением
-       int result =  data[size - 1];//получаем эл массива и сохр в result, чтобы потом вернуть знач result (чтобы показать эл пользователю)
-                //уменьшаем тек размер стека
+
+       int result =  data[size - 1];
         size--;
-        //возвра значение эл, чтобы показать его польз
         return  result;
     }
-
-// доб эл в стек - принимает значение и кладет его на вершину стека
     static void push(int value) {
-       //сначала проверяем, не превышен ли макс размер стека
-        // если превышен отображаем ошибку
         if(size >= max_size) {
             throw new RuntimeException("Stack is full!");
         }
-        //если стек еще не полный кладем новое значение в массив data
-        data[size] = value;
-        //увелич счетчик на 1
 size++;
     }
 
-
-    //позв отобразить самый верхн эл, но не удаляет его
     static int peek() {
         if(size == 0) {
             throw new RuntimeException("Stack is empty!");
         }
-        //сразу же возвр значение польз, записывать в переменную не будем, лишняя строчка кода
        return data[size - 1];
     }
 
